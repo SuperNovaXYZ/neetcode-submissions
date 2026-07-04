83a7ea069643bb0a8fd2b4422819dedc72f07aed
Nova Alim <143953646+SuperNovaXYZ@users.noreply.github.com>
2026-07-03 20:06:30 -0400

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        int[] nums3= new int[nums1.length+nums2.length];
+        int index=0;
+        
+        for(int num : nums1){
+            nums3[index]=num;
+            index++;
+        }
+
+        for(int num : nums2){
+            nums3[index]=num;
+            index++;
+        }
+
+        Arrays.sort(nums3);
+
+
+
+        int n = nums3.length;
+
+        if (n % 2 == 1) {
+            return nums3[n / 2];
+        } else {
+            int mid1 = n / 2 - 1;
+            int mid2 = n / 2;
+            double answer = (double) (nums3[mid1] + nums3[mid2]) / 2;
+            return answer;
+        }
+    }}
