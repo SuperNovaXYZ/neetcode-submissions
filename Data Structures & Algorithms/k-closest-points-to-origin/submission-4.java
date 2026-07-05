7bcbb43d424db845d8affaf6bc176ed1c360e826
Nova Alim <143953646+SuperNovaXYZ@users.noreply.github.com>
2026-07-05 01:39:44 -0400

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        
+        int[][] coord = new int[k][2];
+            
+        PriorityQueue<Point> maxHeap= new PriorityQueue<>((a,b)-> Integer.compare(b.distanceScore,a.distanceScore));
+
+        for(int[]p: points){
+
+        Point currentPoint = new Point(p[0],p[1]);
+
+        maxHeap.offer(currentPoint);
+
+        if(maxHeap.size() > k){
+            maxHeap.poll();
+        }
+
+        }
+
+
+         for (int i = 0; i < k; i++) {
+            Point closest = maxHeap.poll(); 
+            coord[i][0] = closest.x;      
+            coord[i][1] = closest.y;       
+        }
+
+        return coord;
+
+
+
+    }
+
+
+
+}
+class Point{
+    int x;
+    int y;
+    int distanceScore;
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+        this.distanceScore = (x*x) + (y*y);
+
+    }
+}
